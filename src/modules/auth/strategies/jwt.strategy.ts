750a0b7ce3874d24b26ec71c798df34693612183
Juliano Ramalho <juliano_ap_ramalho@hotmail.com>
2026-04-06 21:24:04 -0300

    fix(jwt): correct formatting of secretOrKey assignment

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -15,8 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey:
-        process.env.JWT_SECRET ||
-        'educakids-secret-key-change-in-production',
+        process.env.JWT_SECRET || 'educakids-secret-key-change-in-production',
     });
   }
 
